4058a25d652d5fda6f1655fee1c4a6e5cc9f28a1
XenonYTDE <xenonytde@gmail.com>
2025-01-02 19:51:02 +0100

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -34,7 +34,7 @@ namespace YoutubeDownloader
         private readonly string _historyFilePath;
         private string _lastUrl = string.Empty;
         private readonly UpdateManager _updateManager;
-        private readonly string _currentVersion = "1.1.5"; // Changed from "1.1.3"
+        private readonly string _currentVersion = "1.1.7"; // Changed from "1.1.3"
         private Settings _settings;
         private readonly string _settingsPath;
         private bool _isInitialized;
